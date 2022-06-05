ef78e276139b2733c1b66839f4a7b8e619987372
xukaijie <bajieCloud@163.com>
2022-06-05 11:16:57 +0800

    editor

---
+++ packages/editor/src/graph.ts
@@ -0,0 +1,6 @@
+
+import { Event } from './event';
+
+export class Graph extends Event {
+        
+}
---
+++ packages/editor/src/view.ts
@@ -0,0 +1,8 @@
+
+import {
+    Event
+} from './event'
+
+export class View extends Event {
+    
+}
